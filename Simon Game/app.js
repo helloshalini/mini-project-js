bd5554f2e787d2eef5303dd145b742fe61a351f1
helloshalini <shalinijaiswal1101@gmail.com>
2025-08-28 18:40:42 +0530

    Simon game

--- Simon Game/app.js
+++ Simon Game/app.js
@@ -1,5 +1,5 @@
-let gameSq = [];
-let userSq = [];
+let gameSeq = [];
+let userSeq = [];
 
 let btns = ["red", "yellow", "green", "blue"];
 
@@ -17,7 +17,7 @@ document.addEventListener("keypress", function() {
     }
 });
 
-function btnFlash(btn) {
+function gameFlash(btn) {
     btn.classList.add("flash");
     setTimeout(function() {
         btn.classList.remove("flash");
@@ -34,6 +34,7 @@ function userFlash(btn) {
 
 
 function levelUp() {
+    userSeq = [];
     level++;
     h2.innerText = `Level ${level}`;
 
@@ -41,21 +42,51 @@ function levelUp() {
     let randIdx = Math.floor(Math.random() * 3);
     let randColor = btns[randIdx]
     let randBtn = document.querySelector(`.${randColor}`);
-    console.log(randIdx);
-    console.log(randColor);
-    console.log(randBtn);
-    btnFlash(randBtn);
+    gameSeq.push(randColor);
+    console.log(gameSeq);
+    gameFlash(randBtn);
+
+}
+
+function checkAns(idx) {
+
+    if(userSeq[idx] == gameSeq[idx]) {
+        if(userSeq.length == gameSeq.length) {
+            setTimeout(levelUp, 1000);
+        }
+    }
+    else {
+        h2.innerHTML = `Game Over! Your score was <b>${level}</b> <br> Press any key to start.`;
+        document.querySelector("body").style.backgroundColor = "red";
+        setTimeout(function() {
+           document.querySelector("body").style.backgroundColor = "white";
+        }, 150);
+        reset();
+    }
 }
 
 function btnPress() {
     // console.log("btn was pressed");
-    console.log(this);
+    // console.log(this);
     let btn = this;
     userFlash(btn);
+
+    userColor = btn.getAttribute("id");
+    // console.log(userColor);
+    userSeq.push(userColor);
+
+    checkAns(userSeq.length-1);
 
 }
 
 let allBtns = document.querySelectorAll(".btn");
 for(btn of allBtns) {
     btn.addEventListener("click", btnPress);
+}
+
+function reset() {
+    started = false;
+    gameSeq = [];
+    userSeq = [];
+    level = 0;
 }
